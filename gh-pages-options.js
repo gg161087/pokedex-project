5a8c88a302b2fee7a48adad7c4904f31bd2e0807
gg161087 <gg161087@gmail.com>
2024-01-19 23:22:11 -0300

    fix gh cfg

---
+++ gh-pages-options.js
@@ -0,0 +1,6 @@
+module.exports = {
+    add: true,
+    headers: {
+        'Permissions-Policy': 'interest-cohort=()',
+    },
+};
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/pokedex_project/"
+    plugins: [react()],
+    base: "/pokedex_project/"
 })
